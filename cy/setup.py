8f2ffd8237c29ca8732c5d09508af7f3baa91785
Chris Swierczewski <cswiercz@gmail.com>
2014-11-26 11:56:04 -0800

    Use typed memoryviews for Cython arrays.
    Convert the "old-style" syntax for converting Numpy arrays
    to Cython / C arrays to use memoryviews instead. (See
    http://docs.cython.org/src/userguide/memoryviews.html for
    more information.) This cleans the code and, in some cases,
    can improve performance.
    
    Update the setup script as well.

--- cy/setup.py
+++ cy/setup.py
@@ -2,8 +2,14 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+import numpy
+
 setup(
-    ext_modules = cythonize([Extension("read_mesh", ["read_mesh.pyx"],
-                                        libraries = ["thingy", "m"])
+    ext_modules = cythonize([
+        Extension('read_mesh',
+                  ['read_mesh.pyx', 'thingy.c'],
+                  libraries = ['m'],
+                  include_dirs = [numpy.get_include()],
+              ),
     ])
 )
