b9e58fad68d541efec8f8cbd8a7d5664a3a46d76
Daniel <shapero.daniel@gmail.com>
2014-12-03 15:53:52 -0800

    Changed some names in the 'python-interface' example, which isn't really a python interface anymore

--- python-interface/fe_solver.h
+++ python-interface/fe_solver.h
@@ -1,5 +1,8 @@
 
-void driver(int num_nodes,
+#ifndef FE_SOLVER_H
+#define FE_SOLVER_H
+
+void fe_solve(int num_nodes,
             int num_edges,
             int num_triangles,
             double x[num_nodes],
@@ -7,4 +10,8 @@ void driver(int num_nodes,
             int edges[2 * num_edges],
             int triangles[3 * num_triangles],
             int boundary[num_nodes],
+            double f[num_nodes],
             double u[num_nodes]);
+
+#endif
+
--- python-interface/poisson.c
+++ python-interface/poisson.c
@@ -1,8 +1,9 @@
 
 #include "triangle_meshes.h"
-#include "glue.h"
+#include "fe_solver.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define max(a, b) (a < b) ? b : a;
 #define min(a, b) (a < b) ? a : b;
@@ -18,13 +19,32 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    /* Allocate some vectors */
     double *u = calloc(mesh.num_nodes, sizeof(double));
+    double *f = calloc(mesh.num_nodes, sizeof(double));
 
-    for (int i = 0; i < mesh.num_nodes; i++) u[i] = 0.0;
+    for (int i = 0; i < mesh.num_nodes; i++) {
+        u[i] = 0.0;
 
-    driver(mesh.num_nodes, mesh.num_edges, mesh.num_triangles,
-            mesh.x, mesh.y, mesh.edges, mesh.triangles, mesh.node_boundary, u);
+        /* Fill the RHS vector with some silly trig function */
+        f[i] = sin(mesh.x[i]) * cos(mesh.y[i]);
+    }
 
+    printf("Solving -D^2 u = f, where f(x, y) = sin(x) * cos(y)\n");
+
+    /* Call the Fortran function which uses SiGMA to do all the
+       numerical work */
+    fe_solve(mesh.num_nodes,
+            mesh.num_edges,
+            mesh.num_triangles,
+            mesh.x,
+            mesh.y,
+            mesh.edges,
+            mesh.triangles,
+            mesh.node_boundary,
+            f, u);
+
+    /* Get the minimum and maximum of the solution */
     double mi = 0.0, ma = 0.0;
 
     for (int i = 0; i < mesh.num_nodes; i++) {
@@ -32,10 +52,12 @@ int main(int argc, char **argv) {
         ma = max(ma, u[i]);
     }
 
-    printf("%g %g\n", mi, ma);
+    printf("Minimum and maximum of solution u: (%g, %g)\n", mi, ma);
 
     free(u);
     destroy_mesh(&mesh);
+
+    printf("All done!\n");
 
     return 0;
 }
